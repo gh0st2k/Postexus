b160b83273114e86f6f340806d64b51494696ce5
Maarten Kossen (mpkossen <mpkossen@postexus.com>
2011-08-09 11:31:40 +0200

    Adding database and core classes.

---
+++ core/Core.class.php
@@ -0,0 +1,43 @@
+<?php
+/*************************************************************************
+* Postexus - Modular content management/system platform
+* Copyright (C) 2011 Postexus (gh0st2k, MarcusG, mpkossen, starefossen)
+**************************************************************************
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*************************************************************************/
+
+/*************************************************************************
+* File: core/Core.class.php
+* Author: mpkossen
+*************************************************************************/
+
+class Core
+{
+	static $Database = null;
+
+	/**
+	 * @static Function to get the current Database object
+	 * @return Database object
+	 */
+	public static function getDatabase()
+	{
+		if (is_null($Database)) {
+			self::$Database = new Database();
+		}
+
+		return self::$Database;
+	}
+}
+
+?>
---
+++ core/Database.class.php
@@ -0,0 +1,37 @@
+<?php
+/*************************************************************************
+* Postexus - Modular content management/system platform
+* Copyright (C) 2011 Postexus (gh0st2k, MarcusG, mpkossen, starefossen)
+**************************************************************************
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*************************************************************************/
+
+/*************************************************************************
+* File: core/Database.class.php
+* Author: mpkossen
+*************************************************************************/
+
+class Database
+{
+	public function construct($config = null)
+	{
+		if (is_null($config)) {
+			// Use default config from config file
+		}
+
+		return new mysqli('localhost', 'root', '', 3306);
+	}
+}
+
+?>
